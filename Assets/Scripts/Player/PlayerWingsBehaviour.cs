d221ba6814cbe48a97acdf5cd46d2f6e3c8b5c6a
HBRYU <48374744+HBRYU@users.noreply.github.com>
2023-10-01 00:56:47 +0900

    wing mesh fixed
    issues: mesh stretching

--- Assets/Scripts/Player/PlayerWingsBehaviour.cs
+++ Assets/Scripts/Player/PlayerWingsBehaviour.cs
@@ -18,39 +18,42 @@ public class PlayerWing
     
     public readonly int id;
     public PlayerWingState state;
-    public Transform objectTransform;
     public Material material;
     public Vector3 position;
     public Vector3 idlePosition;
     public Vector3 targetPosition;
     private Transform playerTransform;
+    private Rigidbody2D playerRb;
     public float speed;
     public float range;
     private float travelDistanceCoeff;
 
+    private GameObject meshObject;
     private Mesh mesh;
     private MeshFilter meshFilter;
     private MeshRenderer meshRenderer;
     private List<Vector3> vertices;
 
-    public PlayerWing(int id, Transform objectTransform, Material material, Vector3 idlePosition, float speed, float range, float travelDistanceCoeff)
+    public PlayerWing(int id, GameObject meshObject, Material material, Vector3 idlePosition, float speed, float range, float travelDistanceCoeff)
     {
         this.id = id;
         this.state = PlayerWingState.idle;
-        this.objectTransform = objectTransform;
+
+        this.meshObject = meshObject;
+        
         this.material = material;
         this.idlePosition = idlePosition;
         this.speed = speed;
         this.range = range;
         this.travelDistanceCoeff = travelDistanceCoeff;
         playerTransform = GM.GetPlayer().transform;
+        playerRb = playerTransform.GetComponent<Rigidbody2D>();
 
         position = idlePosition;
-        objectTransform.position = Vector3.zero;
 
         mesh = new Mesh();
-        meshFilter = objectTransform.GetComponent<MeshFilter>();
-        meshRenderer = objectTransform.GetComponent<MeshRenderer>();
+        meshFilter = meshObject.GetComponent<MeshFilter>();
+        meshRenderer = meshObject.GetComponent<MeshRenderer>();
         vertices = new List<Vector3>();
         for (int i = 0; i < 3; i++)
         {
@@ -63,13 +66,12 @@ public class PlayerWing
     public void FollowMouse()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        targetPosition = Vector3.Lerp(playerTransform.position, mousePosition, travelDistanceCoeff);
+        targetPosition = Vector3.Lerp(Vector3.zero, mousePosition - playerTransform.position, travelDistanceCoeff);
         position = Vector3.Lerp(position, targetPosition, speed);
 
         if (Vector3.SqrMagnitude(playerTransform.position - position) > range * range)
         {
-            var position1 = playerTransform.position;
-            position = position1 + (position - position1).normalized * range;
+            position = position.normalized * range;
         }
         
         vertices.RemoveAt(0);
@@ -78,15 +80,15 @@ public class PlayerWing
 
     public void Idle()
     {
-        var position1 = playerTransform.position;
-        var idleWorldPos = idlePosition + position1;
+        Vector3 playerDS = playerRb.velocity * Time.fixedDeltaTime;
+        var idleWorldPos = idlePosition;
         position = idleWorldPos;
 
         Vector3[] vertexTargets = new[]
         {
             idleWorldPos,
             idleWorldPos + new Vector3(-0.1f, 0.2f, 0f),
-            idleWorldPos + new Vector3(-0.3f, 0.6f, 0f),
+            idleWorldPos + new Vector3(-0.3f, 0.5f, 0f),
         };
 
         float[] vertexSpeeds = new[]
@@ -98,6 +100,7 @@ public class PlayerWing
 
         for (int i = 0; i < 3; i++)
         {
+            vertices[i] -= playerDS;
             vertices[i] = Vector3.Lerp(vertices[i], vertexTargets[i], vertexSpeeds[i]);
         }
     }
@@ -112,6 +115,7 @@ public class PlayerWing
 
         Vector3 pos2A = ((pos1 - pos2).normalized + (pos3 - pos2).normalized).normalized * width + pos2;
         Vector3 pos2B = -((pos1 - pos2).normalized + (pos3 - pos2).normalized).normalized * width + pos2;
+        
         
         mesh.vertices = new Vector3[]
         {
@@ -134,6 +138,8 @@ public class PlayerWing
 
 public class PlayerWingsBehaviour : MonoBehaviour
 {
+    public GameObject meshObject;
+    
     public Transform wing1Transform;  // In front of player
     public Transform wing2Transform;  // Behind player
 
@@ -144,7 +150,7 @@ public class PlayerWingsBehaviour : MonoBehaviour
     void Start()
     {
         Vector3 wing1Offset = new Vector3(-0.5f, 0.5f);
-        wing1 = new PlayerWing(1, wing1Transform, wing1Material, wing1Offset, 0.5f, 7f, 0.75f);
+        wing1 = new PlayerWing(1, meshObject, wing1Material, wing1Offset, 0.5f, 7f, 0.75f);
     }
     
     private void FixedUpdate()
