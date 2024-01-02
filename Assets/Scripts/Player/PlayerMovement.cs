08a4dfa0d6a595869706e1e4dbdf9749c7138fff
HBRYU <48374744+HBRYU@users.noreply.github.com>
2024-01-02 22:57:05 +0900

    Player Movement Overhaul
    Fixed messy code

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -19,11 +19,14 @@ public class PlayerMovement : MonoBehaviour
     private PlayerAnimation playerAnimation;
     private delegate void DELVoid();
     private DELVoid flipSprite, move, jump;
+    private bool onGround, prevOnGround;
+    private float onGroundTimer;
 
-    [Header("Dash settings")] 
+    [Header("Dash settings")]
     [SerializeField] private float dashDistance, dashCooldown;
     private float dashCooldownTimer;
     [SerializeField] private float dashYSnap, upDashSpeed;
+    private bool useInputVelocity;
     void Start()
     {
         //Scripts
@@ -47,75 +50,84 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        HandleUseInputX();
+        
         jump();
         Dash();
     }
-    void Empty()
-    {return;}
 
-    // Local Variables for Move()
-    Vector3 input, prevInput, deltaInput;
-    float direction = 1f;
-    void Move()
+    void HandleUseInputX()
     {
-        input = GetInputVector();
-        deltaInput = input - prevInput;
-
-        //----Walk----
-        if (input.x != 0f && prevInput.x == 0f)
-            walkAccelPow = moveAccel;
-        if (input.x == 0f && prevInput.x != 0f)
-            walkAccelPow = stopAccel;
-
-        Vector3 moveVector;
-
-        float vx;
-        if (Input.GetAxisRaw("Horizontal") != 0f)
-            direction = Input.GetAxisRaw("Horizontal");
-        //print(direction);
-        if (walkAccelPow == moveAccel)
-        {
-            vx = Mathf.Clamp(direction * walkSpeed * Mathf.Pow(Mathf.Abs(Input.GetAxis("Horizontal")), walkAccelPow) + direction * v0, -walkSpeed, walkSpeed);
-            moveVector = new Vector3(vx, rb.velocity.y);
-        }
-        else
-        {
-            vx = walkSpeed * Mathf.Pow(Mathf.Abs(Input.GetAxis("Horizontal")), walkAccelPow) * direction;
-            moveVector = new Vector3(vx, rb.velocity.y);
-        }
-
-        rb.velocity = moveVector;
-        RequestAnimation<bool>("Walk", Input.GetAxisRaw("Horizontal") != 0 ? true : false);
-        RequestAnimation<bool>("Walk_play_Decel", Mathf.Abs(vx) >= walkSpeed * 0.8f ? true : false);
-        prevInput = GetInputVector();
-
-        Vector3 GetInputVector()
-        {
-            return new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxis("Vertical")); ;
-        }
+        // Used to determine whether to use x velocity from Input.GetAxis or use its current rb velocity (from dash)
+        
+        onGround = !(Physics2D.OverlapCircle(feetPosition.position, 0.5f, groundLayers) == null);
+        if (onGround && !prevOnGround || onGroundTimer >= 0.5f)
+            useInputVelocity = true;
+        prevOnGround = onGround;
+        if (onGround)
+            onGroundTimer += Time.deltaTime;
     }
 
-    public void Jump()
+    // Local Variables for Move()
+    float prevInput;
+    float direction = 1f;
+    bool usingMoveAccel;
+    void Move()
     {
-        bool onGround = !(Physics2D.OverlapCircle(feetPosition.position, 0.5f, groundLayers) == null);
-        RequestAnimation<bool>("On_Ground", onGround);
+        float inputX = Input.GetAxisRaw("Horizontal");
+        
+        if (inputX != 0f) useInputVelocity = true;
+        
+        if (inputX != 0f && prevInput == 0f)
+        {
+            walkAccelPow = moveAccel;
+            usingMoveAccel = true;
+        }
+
+        if (inputX == 0f && prevInput != 0f)
+        {
+            walkAccelPow = stopAccel;
+            usingMoveAccel = false;
+        }
+
+        float vx = 0f;
+        
+        if (inputX != 0f)
+            direction = inputX;
+        
+        if (usingMoveAccel && useInputVelocity)
+        {
+            vx = Mathf.Clamp(direction * walkSpeed * Mathf.Pow(Mathf.Abs(Input.GetAxis("Horizontal")), walkAccelPow) + direction * v0, -walkSpeed, walkSpeed);
+            rb.velocity = new Vector3(vx, rb.velocity.y);
+        }
+        else if (useInputVelocity)
+        {
+            vx = walkSpeed * Mathf.Pow(Mathf.Abs(Input.GetAxis("Horizontal")), walkAccelPow) * direction;
+            rb.velocity = new Vector3(vx, rb.velocity.y);
+        }
+        RequestAnimation("Walk", inputX != 0);
+        RequestAnimation("Walk_play_Decel", Mathf.Abs(vx) >= walkSpeed * 0.8f);
+        prevInput = inputX;
+    }
+
+    private void Jump()
+    {
+        RequestAnimation("On_Ground", onGround);
 
         if (Input.GetKeyDown(KeyCode.W) && onGround)
         {
             //print("Jump");
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-            RequestAnimation<string>("Jump", string.Empty);
+            RequestAnimation("Jump", string.Empty);
         }
 
     }
 
     // jump without using w key
     public void Free_Jump(){
-        bool onGround = !(Physics2D.OverlapCircle(feetPosition.position, 0.5f, groundLayers) == null);
-
         if (onGround){
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-            RequestAnimation<string>("Jump", string.Empty);
+            RequestAnimation("Jump", string.Empty);
         }
     }
 
@@ -128,46 +140,36 @@ public class PlayerMovement : MonoBehaviour
 
     void Dash()
     {
-        DELVoid dash;
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if (mousePos.y > transform.position.y - dashYSnap && mousePos.y < transform.position.y + dashYSnap)
+        var position = transform.position;
+        Vector3 targetDashPos;
+        
+        if (mousePos.y > position.y - dashYSnap && mousePos.y < position.y + dashYSnap)
         {
-            dash = DashHorizontal;
+            float dashDirectionX = mousePos.x > transform.position.x ? 1f : -1f;
+            float d = dashDistance;
+            targetDashPos = position + new Vector3(dashDirectionX, 0) * d;
         }
         else
         {
-            dash = DashRegular;
+            float d = Vector3.Distance(position, mousePos) >= dashDistance ? dashDistance : Vector3.Distance(position, mousePos);
+            
+            targetDashPos = position + Vector3.Normalize(mousePos - position) * d;
         }
         if (Input.GetKeyDown(KeyCode.F) && dashCooldownTimer <= 0f)
         {
-            dash();
+            rb.MovePosition(targetDashPos);
             dashCooldownTimer = dashCooldown;
             playerAnimation.RequestAnimation("Dash", "Trigger");
-
             
-            sprite.flipX = transform.position.x > mousePos.x;
+            sprite.flipX = position.x > mousePos.x;
             facingRight = !sprite.flipX;
+            rb.velocity = (mousePos - position).normalized * upDashSpeed;
+            useInputVelocity = false;
+            onGroundTimer = 0f;
         }
         else
             dashCooldownTimer -= Time.deltaTime;
-
-        void DashHorizontal()
-        {
-            float direction = mousePos.x > transform.position.x ? 1f : -1f;
-            float d = dashDistance;
-            rb.MovePosition(transform.position + new Vector3(d*direction, 0));
-        }
-        void DashRegular()
-        {
-            float d = Vector3.Distance(transform.position, mousePos) >= dashDistance ? dashDistance : Vector3.Distance(transform.position, mousePos);
-
-            rb.MovePosition(transform.position + Vector3.Normalize(mousePos - transform.position)*d);
-            rb.velocity = Vector3.Normalize(mousePos - transform.position)*upDashSpeed;
-
-            move = Empty;
-        }
-        if (Input.GetAxisRaw("Horizontal") != 0)
-            move = Move;
     }
 
     private void RequestAnimation<T>(string param, T value)
